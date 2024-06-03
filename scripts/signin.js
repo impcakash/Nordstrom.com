85647bd2b3cb114edf96d6c50ab68be0adbad0e1
impcakash <impcakash@gmail.com>
2024-06-03 17:12:06 +0530

    Create signin.js

---
+++ scripts/signin.js
@@ -0,0 +1,8 @@
+function signin(e) {
+    e.preventDefault();
+    let div = document.getElementById("signinbox")
+
+    let myForm = document.getElementById("myForm");
+    let email = myForm.email.value;
+    let password = myForm.password.value;
+}
